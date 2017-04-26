2bd2e2d179a30e4d3bbe66fc3fba739f0b1ae4e8
Sergej Sintschilin <seregpie@gmail.com>
2017-04-26 16:21:10 +0200

    Add files via upload

--- THREE.TextTexture.js
+++ THREE.TextTexture.js
@@ -1,30 +1,35 @@
 (function(THREE) {
 
 	THREE.TextTexture = class extends THREE.Texture {
-		constructor(text = '', fontSize = 16, fontFace = 'serif', ...args) {
+		constructor({
+			text = '',
+			fontSize = 16,
+			fontFace = 'sans-serif',
+			mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy,
+		} = {}) {
 			let canvas = document.createElement('canvas');
 			canvas.getContext('2d');
-			super(canvas, ...args);
+			super(canvas, mapping, wrapS, wrapT, magFilter, minFilter, format, type, anisotropy);
 			this._text = text;
 			this._fontSize = fontSize;
 			this._fontFace = fontFace;
-			this._update();
+			this._updateImage();
 		}
 
-		_update() {
+		_updateImage() {
 			let canvas = this.image;
 			let ctx = canvas.getContext('2d');
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 			let font = `${this._fontSize}px ${this._fontFace}`;
 			ctx.font = font;
 			canvas.width = ctx.measureText(this._text).width;
-			canvas.height = this._fontSize;
+			canvas.height = this._fontSize * 3/2;
 			ctx.font = font;
 			ctx.textAlign = 'center';
 			ctx.textBaseline = 'middle';
 			ctx.fillStyle = 'white';
 			ctx.fillText(this._text, canvas.width / 2, canvas.height / 2);
-			this._aspectRatio = canvas.width / canvas.height;
+			this.aspectRatio = canvas.width / canvas.height;
 			this.needsUpdate = true;
 		}
 
@@ -37,7 +42,7 @@
 				return;
 			}
 			this._text = value;
-			this._update();
+			this._updateImage();
 		}
 
 		get fontSize() {
@@ -49,7 +54,7 @@
 				return;
 			}
 			this._fontSize = value;
-			this._update();
+			this._updateImage();
 		}
 
 		get fontFace() {
@@ -61,11 +66,7 @@
 				return;
 			}
 			this._fontFace = value;
-			this._update();
-		}
-
-		get aspectRatio() {
-			return this._aspectRatio;
+			this._updateImage();
 		}
 	};
 
